2710bfca9875d1a6e057fc3b347e38aa34199936
Geoffrey Floyd <geoffrey.floyd@hoomanlogic.com>
2017-03-05 10:14:39 -0700

    clean up flowcrumb style

--- app/components/FlowCrumb/index.js
+++ app/components/FlowCrumb/index.js
@@ -7,8 +7,10 @@ import { Icon } from 'react-native-elements';
 
 function FlowCrumb ({ theme, title, subtitle, onPress }) {
     return (
-        <View style={{ padding: 8, flex: 1, flexDirection: 'row' }}>
-            <Icon name="chevron-left" size={32} color={theme.foreColor} onPress={onPress}/>
+        <View style={{ padding: 8, height: subtitle ? 70 : 50, flex: 1, flexDirection: 'row', borderBottomColor: theme.bgColorLow, borderBottomWidth: 2 }}>
+            <View style={{ paddingRight: 8, paddingBottom: 8, paddingTop: subtitle ? 8 : 4 }}>
+                <Icon name="keyboard-backspace" size={32} color={theme.foreColor} onPress={onPress}/>
+            </View>
             <View style={{ flex: 1 }}>
                 <Text style={{ color: theme.foreColor, fontSize: 24 }}>{title}</Text>
                 {subtitle ? <Text style={{ color: theme.foreColorLow, fontSize: 12 }}>{subtitle}</Text> : undefined}
